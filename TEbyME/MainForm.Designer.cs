1aaf8eff61eb4327b71634118c338b9d8121da3d
albertkrdoyan <albert.krdoyan1@gmail.com>
2024-12-16 18:30:36 +0400

    X 16.12.2024 18:30 new version update

--- TEbyME/MainForm.Designer.cs
+++ TEbyME/MainForm.Designer.cs
@@ -510,7 +510,7 @@ namespace TEbyME
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Text Editor by M.E. | V2.0";
+            this.Text = "Text Editor by M.E. | V2.1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.SizeChanged += new System.EventHandler(this.Sform_sizeeventhandler);
